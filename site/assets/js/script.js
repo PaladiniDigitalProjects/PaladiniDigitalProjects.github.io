db4d153494f359a6af80e4f9808176f8d2da2fc8
Paladini Digital Projects <daniel@paladini.cat>
2018-05-08 14:24:20 +0200

    js

--- site/assets/js/script.js
+++ site/assets/js/script.js
@@ -1,12 +1,9 @@
 $(document).ready(function(){
-
 	$menuLeft = $('.pushmenu-left');
 	$nav_list = $('#nav_list');
-
 	$nav_list.click(function() {
 		$(this).toggleClass('active');
 		$('.pushmenu-push').toggleClass('pushmenu-push-toright');
 		$menuLeft.toggleClass('pushmenu-open');
 	});
-  $console.log('page --> loaded');
 });
