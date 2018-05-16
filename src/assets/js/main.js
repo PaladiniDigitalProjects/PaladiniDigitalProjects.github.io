3b108e53ab06853ae2589c3d704e3d529a52df11
Paladini Digital Projects <daniel@paladini.cat>
2018-05-16 16:59:58 +0200

    Imagen pesada + Lazy Load

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -17,4 +17,11 @@ $(document).ready(function(){
         }
     });
 	});
+
+	[].forEach.call(document.querySelectorAll('img[data-src]'),
+	function(img) {
+  	img.setAttribute('src', img.getAttribute('data-src'));
+  	img.onload = function() { img.removeAttribute('data-src');
+		};
+	});
 });
