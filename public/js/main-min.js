b10ef5b39efc875310bb33724154f5e103494615
Paladini Digital Projects <daniel@paladini.cat>
2018-05-29 14:50:07 +0200

    All

--- public/js/main-min.js
+++ public/js/main-min.js
@@ -24,6 +24,14 @@ $(document).ready(function(){
   	img.onload = function() { img.removeAttribute('data-src');
 		};
 	});
+
+	(function() { // DON'T EDIT BELOW THIS LINE
+	  var d = document, s = d.createElement('script');
+	  s.src = 'https://paladinidigitalprojets.disqus.com/embed.js';
+	  s.setAttribute('data-timestamp', +new Date());
+	  (d.head || d.body).appendChild(s);
+	  })();
+
 });
 
 
