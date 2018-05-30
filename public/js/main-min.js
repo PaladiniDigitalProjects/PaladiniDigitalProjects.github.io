1399246dc851ed76064cd01de31e6050e993c166
Paladini Digital Projects <daniel@paladini.cat>
2018-05-30 18:28:02 +0200

    Definiciones

--- public/js/main-min.js
+++ public/js/main-min.js
@@ -32,6 +32,35 @@ $(document).ready(function(){
 	  (d.head || d.body).appendChild(s);
 	  })();
 
+
+	// CARROUSELL
+
+	$( "<div class='slider-counter'></div>" ).appendTo( ".slider" );
+
+	$('.owl-slider').on('initialized.owl.carousel changed.owl.carousel', function(e) {
+    if (!e.namespace)  {
+      return;
+    }
+		var carousel = e.relatedTarget;
+    $('.slider-counter').text(carousel.relative(carousel.current()) + 1 + '/' + carousel.items().length);
+  }).owlCarousel({
+		items:1,
+		loop:true,
+		margin:0,
+		autoPlay:true,
+		autoplayTimeout:1000,
+		autoplayHoverPause:true,
+		autoHeight:true,
+		nav:true,
+		navText: ["<img class='img-svg' src='public/images/prev-ico.svg'>","<img class='img-svg' src='public/images/next-ico.svg'>"]
+  });
+
+
+
+
+
+
+
 });
 
 
